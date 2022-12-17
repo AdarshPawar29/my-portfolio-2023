eb7a1fd3f57b87bd63d5e2e2f7a0ed5e33f49d3f
Adarsh Pawar <adarshpawar@Adarshs-MacBook-Pro.local>
2022-12-17 21:10:04 +0530

    1️⃣ commit

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="container">
+        <div className="roww">
+          {/* Main Title */}
+          <div className="resumo_fn_main_title">
+            <h3 className="subtitle">Introduction</h3>
+            <h3 className="title">Software Engineer</h3>
+            <p className="desc">
+              Software engineer proficient in JavaScript, React, Node, Express,
+              Python and Deno. I have 3 year experience as a front end
+              development. Talking about history I had major in Data Science in
+              University.
+            </p>
+            <img src="img/signature.png" alt="image" />
+          </div>
+          {/* /Main Title */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
